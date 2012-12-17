a90acb3aea0d7e6b183c3226388655e32e5604f9
yahagi tomoaki <yahagi@fact-real.com>
2012-12-17 15:45:04 +0900

    setup-aliases-all.sh: Add bash_profile comment

--- setup-aliases-all.sh
+++ setup-aliases-all.sh
@@ -3,6 +3,8 @@ target_rc=~/.bash_profile
 env_dir=~/my_env
 aliases_dir=$env_dir/aliases
 
+echo
+echo "# Append Aliases by https://github.com/myun2/my_env"
 echo "source $aliases_dir/others" >> $target_rc
 echo "source $aliases_dir/git" >> $target_rc
 echo "source $aliases_dir/tmux" >> $target_rc
