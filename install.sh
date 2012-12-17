c9cc6b9c2fffacac33f1cedafc7b5591afffffba
yahagi tomoaki <yahagi@fact-real.com>
2012-12-17 16:02:41 +0900

    install.sh: Add Static Link

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+repos_dir=~/my_env/
 target_rc=~/.bash_profile
 cat aliases/_bash_profile >> $target_rc
+
+# Static Links
+ln -s $repos_dir/_emacs ~/.emacs
+ln -s $repos_dir/_gitconfig ~/.gitconfig
+ln -s $repos_dir/_tmux.conf ~/.tmux.conf
+ln -s $repos_dir/_vimrc ~/.vimrc
