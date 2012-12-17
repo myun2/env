45b9eec37c2d2ae47dfb6d24e0fc4de43f00c5a4
myun2 <myun2@nwhite.info>
2012-12-17 16:18:02 +0900

    install.sh: override static link file (interactive)

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-repos_dir=~/my_env/
+repos_dir=~/my_env
 dotfiles_dir=$repos_dir/dotfiles
 target_rc=~/.bash_profile
 cat aliases/_bash_profile >> $target_rc
 
 # Static Links
-ln -s $dotfiles_dir/emacs ~/.emacs
-ln -s $dotfiles_dir/gitconfig ~/.gitconfig
-ln -s $dotfiles_dir/tmux.conf ~/.tmux.conf
-ln -s $dotfiles_dir/vimrc ~/.vimrc
+ln -is $dotfiles_dir/emacs ~/.emacs
+ln -is $dotfiles_dir/gitconfig ~/.gitconfig
+ln -is $dotfiles_dir/tmux.conf ~/.tmux.conf
+ln -is $dotfiles_dir/vimrc ~/.vimrc
