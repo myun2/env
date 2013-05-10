36db0b6219ec4140fc89c841d100d2918ac35eca
Tomoaki Yahagi <yahagi@fact-real.com>
2013-05-10 18:21:40 +0900

    fixes

--- install.sh
+++ install.sh
@@ -6,30 +6,36 @@ target_rc=~/.bash_profile
 
 echo -n "Append aliases to your .bash_profile ? (y/n) >"
 read input
-if [ $ipunt = 'y' ] || [ $ipunt = 'Y' ]; then
+echo "$input"
+if [ "$input" = "y" ] || [ "$input" = "Y" ]; then
+  echo cat aliases/_bash_profile >> $target_rc
   cat aliases/_bash_profile >> $target_rc
 fi
 
 echo -n "Static Link ~/.vimrc -> $dotfiles_dir/vimrc ? (y/n) >"
 read input
-if [ $ipunt = 'y' ] || [ $ipunt = 'Y' ]; then
+if [ $input = 'y' ] || [ $input = 'Y' ]; then
+  echo ln -is $dotfiles_dir/vimrc ~/.vimrc
   ln -is $dotfiles_dir/vimrc ~/.vimrc
 fi
 
 echo -n "Static Link ~/.gitconfig -> $dotfiles_dir/gitconfig ? (y/n) >"
 read input
-if [ $ipunt = 'y' ] || [ $ipunt = 'Y' ]; then
+if [ $input = 'y' ] || [ $input = 'Y' ]; then
+  echo ln -is $dotfiles_dir/gitconfig ~/.gitconfig
   ln -is $dotfiles_dir/gitconfig ~/.gitconfig
 fi
 
 echo -n "Static Link ~/.tmux.conf -> $dotfiles_dir/tmux.conf ? (y/n) >"
 read input
-if [ $ipunt = 'y' ] || [ $ipunt = 'Y' ]; then
+if [ $input = 'y' ] || [ $input = 'Y' ]; then
+  echo ln -is $dotfiles_dir/tmux.conf ~/.tmux.conf
   ln -is $dotfiles_dir/tmux.conf ~/.tmux.conf
 fi
 
 echo -n "Static Link ~/.emacs -> $dotfiles_dir/emacs ? (y/n) >"
 read input
-if [ $ipunt = 'y' ] || [ $ipunt = 'Y' ]; then
+if [ $input = 'y' ] || [ $input = 'Y' ]; then
+  echo ln -is $dotfiles_dir/emacs ~/.emacs
   ln -is $dotfiles_dir/emacs ~/.emacs
 fi
