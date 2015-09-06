58c3c7a91093b9cc68c9ed60a38e3e361c518387
myun2 <myun2@nwhite.info>
2015-09-06 17:11:56 +0900

    packages-install.sh add

---
+++ scripts/packages-install.sh
@@ -0,0 +1,7 @@
+sudo yum update
+sudo yum -y install gcc gcc-c++ g++ wget
+sudo yum -y install git tmux
+sudo yum -y install openssl-devel gem lv ruby-devel libxml2-devel libxslt-devel
+sudo yum -y install libyaml-devel readline-devel libffi-devel sqlite-devel
+sudo yum -y install perl-ExtUtils-MakeMaker
+sudo yum -y install curl-devel expat-devel gettext-devel openssl-devel zlib-devel
